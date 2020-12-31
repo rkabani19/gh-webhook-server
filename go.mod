722e0e0b1dc340c3995bb1d6311b383fa753fd5f
rkabani19 <rohail.kabani@gmail.com>
2020-12-31 15:24:10 -0500

    handle push, pr, and watch events

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/rkabani19/gh-webhook-server
 go 1.14
 
 require (
-	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
 )
--- webhook/webhook.go
+++ webhook/webhook.go
@@ -1,22 +1,42 @@
 package webhook
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/google/go-github/github"
 )
 
 func HandleWebhook(w http.ResponseWriter, r *http.Request) {
-	// TODO: change to struct
-	webhookData := make(map[string]interface{})
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("error reading request body: err=%s\n", err)
+		return
+	}
+	defer r.Body.Close()
 
-	if err := json.NewDecoder(r.Body).Decode(&webhookData); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		log.Printf("could not parse webhook: err=%s\n", err)
 		return
 	}
 
-	fmt.Println("acquired webhook payload: ")
-	for k, v := range webhookData {
-		fmt.Printf("%s: %v\n", k, v)
+	HandleEvent(event)
+}
+
+func HandleEvent(event interface{}) {
+	switch e := event.(type) {
+	case *github.PushEvent:
+		fmt.Println("Push event.")
+	case *github.PullRequestEvent:
+		fmt.Println("Pull Request event.")
+	case *github.WatchEvent:
+		if e.Action != nil && *e.Action == "starred" {
+			fmt.Printf("%s starred repository %s\n", *e.Sender.Login, *e.Repo.FullName)
+		}
+	default:
+		log.Printf("Event type %s unknown\n", e)
 	}
 }
